2cd2d9bef7cbe706793cb518b5c9cc144418bbd2
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-11-25 23:28:04 +0530

    Add playback and volume controls to audio player
    Implemented playback bar for seeking and displaying current time and duration. Added volume bar for adjusting audio volume. Included helper function to format time display.

--- Spotify Clone/app.js
+++ Spotify Clone/app.js
@@ -15,3 +15,51 @@ playBtn.addEventListener("click", () => {
     playBtn.src = "./assets/player_icon3.png"; // your play icon
   }
 });
+
+
+const playbackBar = document.querySelector(".playback-bar");
+const currentTimeText = document.querySelectorAll(".time")[0];
+const volumeBar = document.querySelector(".volumn-bar");
+
+// default volume
+audio.volume = 0.8;
+
+volumeBar.addEventListener("input", () => {
+    audio.volume = volumeBar.value / 100;
+});
+
+const durationText = document.querySelectorAll(".time")[1];
+
+// When audio meta is loaded, show duration
+audio.addEventListener("loadedmetadata", () => {
+  playbackBar.max = audio.duration;
+  durationText.textContent = formatTime(audio.duration);
+});
+
+// Update bar while playing
+audio.addEventListener("timeupdate", () => {
+  playbackBar.value = audio.currentTime;
+  currentTimeText.textContent = formatTime(audio.currentTime);
+});
+
+// Seek
+playbackBar.addEventListener("input", () => {
+  audio.currentTime = playbackBar.value;
+});
+
+// Format time (helper function)
+function formatTime(sec) {
+  let m = Math.floor(sec / 60);
+  let s = Math.floor(sec % 60);
+  if (s < 10) s = "0" + s;
+  return `${m}:${s}`;
+}
+
+const volumebar = document.querySelector(".volumn-bar");
+
+// default volume
+audio.volume = 0.8;
+
+volumebar.addEventListener("input", () => {
+    audio.volume = volumeBar.value / 100;
+});
